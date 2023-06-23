a7339cb55e703f423e0e3dbefc0de9bb96a0ee97
jubairJnu <jubair2810@gmail.com>
2023-06-23 23:18:38 +0600

    added-header

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Shared/Header';
+
+const Main = () => {
+  return (
+    <div>
+      <h3 className='text-danger' >this is main</h3>
+      <Header></Header>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Shared/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <div>
+       <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="mx-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Blog</Nav.Link>      
+          </Nav>
+          
+            <Button variant="outline-success">Search</Button>
+       
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  );
+};
+
+export default Header;
